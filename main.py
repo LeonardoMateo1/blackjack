1d5fbba6e32f6bf1b1e51907415c9134b6d5c0c9
Leonardo Mateo <leom305131@gmail.com>
2025-08-24 20:37:05 -0400

    Added dealer, score ability to Hit, Stand, Bust, and Blackjack

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ class deck:
 class hand:
     def __init__(self, cards):
         self.cards = cards
+        self.card_list = [cards[0], cards[1]]
 
     def add(self, card):
-        self.cards.append(card)
+        self.cards.extend(card)
     
     def score(self) -> int:
         score = 0
@@ -36,6 +37,8 @@ class hand:
                 score += int(card[0])
         return score
 
+        
+
     def __str__(self):
         return str(self.cards)
     
@@ -47,6 +50,10 @@ class player:
     def __str__(self):
         return str(self.name)
     
+class dealer:
+    def __init__(self, hand):
+        self.hand = hand
+    
 class game:
     def __init__(self, num_players):
         self.num_players = num_players
@@ -54,15 +61,52 @@ class game:
         d = deck()
         d.shuffle()
 
+        self.dealer = dealer(hand(d.draw(2)))
+
         self.players = [player(input(f"Enter player {i+1} name: "), hand(d.draw(2))) for i in range(self.num_players)]
 
     def display_players(self):
+        print("Dealer")
+        print(self.dealer.hand.card_list[0][0] + " of " + self.dealer.hand.card_list[0][1])
+        print("--" * 5)
+
         for player in self.players:
             print(player.name.title())
             for card in player.hand.cards:
                 print(card[0] + " of " + card[1])
             print(f"Current score: {player.hand.score()}")
             print("--" * 5)
+
+    def play(self):
+        d = deck()
+
+        for player in self.players:
+            print(player.name.title())
+            if player.hand.card_list[0][0] == player.hand.card_list[1][0]:
+                print("(Split, Hit, Stand)")
+            else:
+                print("(Hit, Stand)")
+            for card in player.hand.cards:
+                print(card[0] + " of " + card[1])
+            print(f"Current score: {player.hand.score()}")
+
+            while True:
+                if player.hand.score() == 21:
+                    print("Blackjack!")
+                    break
+                elif player.hand.score() > 21:
+                    print("Bust!")
+                    break
+                else:
+                    self.play = input("What would you like to do? ")
+
+                    if self.play == "Hit":
+                        player.hand.add(d.draw(1))
+                        print(player.hand)
+                        print(f"New score: {player.hand.score()}")
+                    elif self.play == "Stand":
+                        break
+
 
 if __name__ == "__main__":
     print("Welcome to Blackjack!")
@@ -71,3 +115,4 @@ if __name__ == "__main__":
     print("--" * 5)
 
     game.display_players()
+    game.play()
